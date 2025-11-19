b38a99c31eb959e54fb96e36a9bb05836e29004b
heistermann <maik.heistermann@uni-potsdam.de>
2025-11-19 12:06:09 +0100

    add requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,8 @@
 # limitations under the License.
 
 import streamlit as st
-#import yfinance as yf
 import pandas as pd
 import numpy as np
-import altair as alt
 import plotly.express as px
 import plotly.graph_objects as go
 
